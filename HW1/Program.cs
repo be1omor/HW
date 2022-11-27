c060afd6687232d5f02c1ba19d7d02de02cf40f3
Ilya Lyalin <lyalin315@gmail.com>
2022-11-27 23:37:54 +0300

    dz1.1

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите 2 числа:");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int min = 0;
+int max = 0;
+
+if (a>b)
+{
+    max = a;
+    min = b;
+    Console.WriteLine($"Max = {max}, Min = {min}");
+}
+else
+{
+    max = b;
+    min = a;
+    Console.WriteLine($"Max = {max}, Min = {min}");
+}
